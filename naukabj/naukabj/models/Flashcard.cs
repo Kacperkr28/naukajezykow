942db1c4ff5d1f22f3906cf34ef87a659c674509
Kacperkr28 <krawczyk.kacper@zst.radom.pl>
2024-12-09 15:49:04 +0100

    Delete naukabj/naukabj/models directory

--- naukabj/naukabj/models/Flashcard.cs
+++
@@ -1,7 +0,0 @@
-public class Flashcard
-{
-    public string Question { get; set; }
-    public string Answer { get; set; }
-    public int CorrectAttempts { get; set; }
-    public int IncorrectAttempts { get; set; }
-}
